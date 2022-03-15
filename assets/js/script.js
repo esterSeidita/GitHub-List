63cd5b08c9f6da82e8aa281b518a73d0162c1f97
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-03-15 18:46:40 +0100

    fix: animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 const q = (attribute) => document.querySelector(attribute);
-const popup = q('#popup');
-const links = document.querySelectorAll('a');
+const popup = q("#popup");
+const links = document.querySelectorAll("a");
 
 /* -------------------------------------------------------------------------- */
 /*                       Funzione per generare il popup                       */
 /* -------------------------------------------------------------------------- */
 
-function popupGenerator (link, index){
-    let githubLink = link[index].getAttribute("data-git");
-    let linkedinLink = link[index].getAttribute("data-linkedin");
+function popupGenerator(link, index) {
+  let githubLink = link[index].getAttribute("data-git");
+  let linkedinLink = link[index].getAttribute("data-linkedin");
 
-    popup.classList.add('hidden');
-    popup.innerHTML = `
+  popup.classList.add("hidden");
+  popup.innerHTML = `
     <img src="./assets/img/closeBtn.png" class="closeBtn">
     <img src="./assets/img/rightArrow.png" class="moveToRight">
     <img src="./assets/img/leftArrow.png" class="moveToLeft">
@@ -19,7 +19,7 @@ function popupGenerator (link, index){
     <h1>${link[index].textContent}</h1>
     <h2>Scegli la piattaforma:</h2>
     <div class="groupBtn">
-    <a href="${githubLink}" target="_blank">
+    <a href="${githubLink}" target="_blank" class="githubBtn">
     <button>GitHub</button>
     </a>
     <a href="${linkedinLink}" target="_blank" class="linkedinBtn">
@@ -27,41 +27,40 @@ function popupGenerator (link, index){
     </a>
     </div>
     `;
-    if(linkedinLink === ''){
-        q('.linkedinBtn').classList.add('inactive');
-        const att = q('.linkedinBtn button').setAttribute("title", "Linkedin non presente");
-    };
+  if (linkedinLink === "") {
+    q(".linkedinBtn").classList.add("inactive");
+    const att = q(".linkedinBtn button").setAttribute(
+      "title",
+      "Linkedin non presente"
+    );
+  }
 
-    // Close Button
-    const closeBtn = q('.closeBtn');
-    
-    closeBtn.addEventListener("click", () => {
-        popup.classList.remove('hidden');
-        popup.innerHTML = '';
-    })
+  // Close Button
+  const closeBtn = q(".closeBtn");
 
-    // Arrow Buttons
-    const leftBtn = q('.moveToLeft');
-    const rightBtn = q('.moveToRight');
-    
-    leftBtn.addEventListener("click", () => {
-        index --;
-        popupGenerator(link, index)
+  closeBtn.addEventListener("click", () => {
+    popup.classList.remove("hidden");
+    popup.innerHTML = "";
+  });
 
-    })    
-    rightBtn.addEventListener("click", () => {
-        index ++
-        popupGenerator(link, index)
-    })
+  // Arrow Buttons
+  const leftBtn = q(".moveToLeft");
+  const rightBtn = q(".moveToRight");
 
+  leftBtn.addEventListener("click", () => {
+    index--;
+    popupGenerator(link, index);
+  });
+  rightBtn.addEventListener("click", () => {
+    index++;
+    popupGenerator(link, index);
+  });
 }
 
 //Fine funzione
 
 links.forEach((link, i, allLink) => {
-    allLink[i].addEventListener('click', () => {
-
+  allLink[i].addEventListener("click", () => {
     popupGenerator(allLink, i);
-
-    })
-})
+  });
+});
