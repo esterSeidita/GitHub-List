b08d2ae88b71c1308cf39ed51118b956456574fe
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-03-15 17:39:59 +0100

    fit: arrow interactions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,67 @@
-const popup = document.querySelector('#popup');
+const q = (attribute) => document.querySelector(attribute);
+const popup = q('#popup');
 const links = document.querySelectorAll('a');
 
-links.forEach(link => {
-    let githubLink = link.getAttribute("data-git");
-    let linkedinLink = link.getAttribute("data-linkedin");
-    link.addEventListener('click', () => {
-        popup.classList.add('hidden');
-        popup.innerHTML = `
-        <div class="closeBtn">X</div>
-        <h1>${link.textContent}</h1>
-        <h2>Scegli la piattaforma:</h2>
-        <div class="groupBtn">
-        <a href="${githubLink}" target="_blank">
-        <button>GitHub</button>
-        </a>
-        <a href="${linkedinLink}" target="_blank" class="linkedinBtn">
-        <button>Linkedin</button>
-        </a>
-        </div>
-        `;
-        if(linkedinLink === ''){
-            document.querySelector('.linkedinBtn').classList.add('inactive');
-            const att = document.querySelector('.linkedinBtn button').setAttribute("title", "Linkedin non presente");
-        };
-        
-        const closeBtn = document.querySelector('.closeBtn');
-        closeBtn.addEventListener("click", () => {
-            popup.classList.remove('hidden');
-            popup.innerHTML = '';
-        })
+/* -------------------------------------------------------------------------- */
+/*                       Funzione per generare il popup                       */
+/* -------------------------------------------------------------------------- */
+
+function popupGenerator (link, index){
+    let githubLink = link[index].getAttribute("data-git");
+    let linkedinLink = link[index].getAttribute("data-linkedin");
+
+    popup.classList.add('hidden');
+    popup.innerHTML = `
+    <img src="./assets/img/closeBtn.png" class="closeBtn">
+    <img src="./assets/img/rightArrow.png" class="moveToRight">
+    <img src="./assets/img/leftArrow.png" class="moveToLeft">
+    
+    <h1>${link[index].textContent}</h1>
+    <h2>Scegli la piattaforma:</h2>
+    <div class="groupBtn">
+    <a href="${githubLink}" target="_blank">
+    <button>GitHub</button>
+    </a>
+    <a href="${linkedinLink}" target="_blank" class="linkedinBtn">
+    <button>Linkedin</button>
+    </a>
+    </div>
+    `;
+    if(linkedinLink === ''){
+        q('.linkedinBtn').classList.add('inactive');
+        const att = q('.linkedinBtn button').setAttribute("title", "Linkedin non presente");
+    };
+
+    // Close Button
+    const closeBtn = q('.closeBtn');
+    
+    closeBtn.addEventListener("click", () => {
+        popup.classList.remove('hidden');
+        popup.innerHTML = '';
+    })
+
+    // Arrow Buttons
+    const leftBtn = q('.moveToLeft');
+    const rightBtn = q('.moveToRight');
+    
+    leftBtn.addEventListener("click", () => {
+        index --;
+        popupGenerator(link, index)
+
+    })    
+    rightBtn.addEventListener("click", () => {
+        index ++
+        popupGenerator(link, index)
+    })
+
+}
+
+//Fine funzione
+
+links.forEach((link, i, allLink) => {
+    allLink[i].addEventListener('click', () => {
+
+    popupGenerator(allLink, i);
+
     })
 })
